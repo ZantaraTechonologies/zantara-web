22bf44cdaffa2c1db2f37605c289d640d8b5e851
Dahiru Aliyu Adamu <dahirualiyuadamu@gmail.com>
2026-03-28 11:24:04 +0100

    Fix incorrect import path in apiClient.js

--- src/services/api/apiClient.js
+++ src/services/api/apiClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useAuthStore } from '../store/auth/authStore';
+import { useAuthStore } from '../../store/auth/authStore';
 
 const API = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
